5274c47b1467062fb64aade2cca8ecbb5a0e3168
red0825 <65748633+red0825@users.noreply.github.com>
2022-01-25 15:43:12 +0900

    Add files via upload

---
+++ 1402_A번난이도_easy.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int t, a, b;
+    cin >> t;
+    for(int i = 0; i < t ; i++){    //발상의 전환이 필요함... (-1*-1*1 = 1, -1*1 = 1) 덧셈으로 모든 수 표현 가능!!!
+        cin >> a >> b;
+        cout << "yes\n";
+    }
+}
