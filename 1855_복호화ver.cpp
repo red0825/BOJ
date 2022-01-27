319ecd32e830c7b6d749a0a6f56d8c50af4ac8a7
red0825 <65748633+red0825@users.noreply.github.com>
2022-01-27 15:00:55 +0900

    Add files via upload

---
+++ 1855_복호화ver.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define MAX 200
+
+using namespace std;
+
+char report[MAX][MAX];
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int n, cnt = 0;
+    string s;
+    cin >> n >> s;
+    for(int i = 0; i < (s.length()/n); i++){
+        if(i%2 == 0){
+            for(int j = 0; j < n; j++) report[i][j] = s[cnt++];
+        }
+        else{
+            for(int j = n-1; j >= 0; j--) report[i][j] = s[cnt++];
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < (s.length()/n); j++) cout << report[j][i];
+    }
+
+}
